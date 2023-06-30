09122d9467830fe21524164c230dbe368f594320
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-06-30 11:38:13 -0300

    Atualizando o exercicio 4

--- src/main/kotlin/ExQuatro/ExercicioQuatro.kt
+++ src/main/kotlin/ExQuatro/ExercicioQuatro.kt
@@ -9,9 +9,9 @@ class ExercicioQuatro {
     fun medirTemperatura(temperatura: Double) {
         if (temperatura <= 18) {
             println("O clima está frio")
-        } else if (temperatura >= 19 && temperatura <= 23) {
+        } else if (temperatura in 19.0..23.0) {
             println("O clima está agradavel")
-        } else if (temperatura >= 24 && temperatura <= 34) {
+        } else if (temperatura in 24.0..34.0) {
             println("O clima está quente")
         } else {
             println("O clima está muito quente")
